5f75a14d465d82d94ee6db9550936af48e2f6dd2
koowei23 <125952248+Anpupupi@users.noreply.github.com>
2023-07-18 00:33:10 +0800

    Delete src directory

--- src/dynamic/SortByStartTime.java
+++
@@ -1,9 +0,0 @@
-package dynamic;
-import java.util.Comparator;
-
-//sort job in ascending order as per start time
-public class SortByStartTime implements Comparator<Job>{
-	public int compare(Job a, Job b) {
-		return a.startTime - b.startTime;
-	}
-}
